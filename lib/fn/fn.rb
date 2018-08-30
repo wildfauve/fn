8cc3575c8ab8f569f4603fc225534a07091b0004
Col Perks <wild.fauve@gmail.com>
2018-08-31 07:33:17 +1200

    add fn

--- lib/fn/fn.rb
+++ lib/fn/fn.rb
@@ -185,7 +185,7 @@ module Fn
       # Returns the result of f, otherwise nil
       # > lift_value.(maybe_value_ok?, maybe_value),
       def lift_monad
-        -> value { maybe_value_ok?.(value) ? maybe_value_ok?.(value) : maybe_fail_value.(value) }
+        -> value { maybe_value_ok?.(value) ? maybe_value.(value) : maybe_failure.(value) }
       end
 
 
@@ -201,12 +201,16 @@ module Fn
         ->(v) { v.success? }
       end
 
+      def maybe_value_fail?
+        -> v { v.failure }
+      end
+
       def maybe_value
         ->(v) { v.value_or }
       end
 
-      def maybe_value_fail?
-        -> v { v.failure }
+      def maybe_failure
+        ->(v) { v.failure }
       end
 
       def status_value_ok?
